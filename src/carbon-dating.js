a4004778a926d57d0ea6e8837cf2dde10c210c1f
Anastasia Prokofieva <anast.ars@yandex.ru>
2020-10-30 20:47:04 +0400

    feat: add carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    // throw new CustomError('Not implemented');
+    let ln = 0.693;
+    if (typeof sampleActivity === "string") {
+        if (+sampleActivity%1 === 0 && +sampleActivity>0 && +sampleActivity<MODERN_ACTIVITY ) {
+            return Math.ceil(MODERN_ACTIVITY / +sampleActivity) / (ln / HALF_LIFE_PERIOD)
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
 };
