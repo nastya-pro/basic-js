e1920bd5f938960963cb58ba1f668947549083d5
Anastasia Prokofieva <anast.ars@yandex.ru>
2020-10-30 20:46:30 +0400

    feat: add count-cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    // throw new CustomError('Not implemented');
+    let num = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (String(matrix[i][j]) === "^^") {
+                num = num +1;
+            }
+        }
+    }
+    return num;
 };
