402895d0a612f1d249c2c5e4f472771d72c69238
Anastasia Prokofieva <anast.ars@yandex.ru>
2020-11-01 04:31:16 +0400

    feat: add transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let array = arr.slice();
+    if (typeof arr !== typeof array) {
+        throw Error
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (array[i] === "--discard-next") {
+            array[i] = null;
+            if (i !== arr.length - 1) {
+                array[i + 1] = null
+            }
+        } else if (array[i] === "--discard-prev") {
+            array[i] = null;
+            if (i !== 0) {
+                array[i - 1] = null
+            }
+        } else if (array[i] === "--double-next") {
+            if (i !== arr.length - 1) {
+                array[i] = array[i + 1];
+            } else {
+                array[i] = null;
+            }
+        } else if (array[i] === "--double-prev") {
+            if (i !== 0) {
+                array[i] = array[i - 1];
+            } else {
+                array[i] = null;
+            }
+        }
+    }
+    return array.filter(el => el !== null);
 };
+
